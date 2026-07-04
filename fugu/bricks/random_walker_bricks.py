dd6295b932aae328fc453d0a580ecc0130a9c450
merttarim <merttarim1@gmail.com>
2026-07-04 15:17:01 -0400

    working brick and tutorial

--- fugu/bricks/random_walker_bricks.py
+++ fugu/bricks/random_walker_bricks.py
@@ -1,206 +1,214 @@
-import ast
-import logging
-
-import networkx as nx
 import numpy as np
-from numpy import genfromtxt
-from scipy.io import loadmat
-
-from .bricks import Brick, input_coding_types
-from .input_bricks import Vector_Input
-from ..scaffold import Scaffold
-from . import spiking_pde as SpikingPDE
-
-rw_logger = logging.getLogger("NRW")
-
-class DensityRW(Brick):
-    """Basic Density Random Walk Circuit
-    
-    Arguments:
-        + timesteps - An integer number of timesteps to run the walk
-        + transitions - A dictionary of valid transition edges on the graph {source_node:neighbors}, 
-                        neighbors should be a list of tuples (destination_node, probability)
-        + init_walkers - A dictionary {start_node: number_of_walkers} to initialize the walk
-    
-    """
-    def __init__(self, 
-                 timesteps, 
-                 transitions, 
-                 init_walkers={(10,):30, (13,): 30}, 
-                 name=None, 
-                 coding='Raster'):
-        super(Brick, self).__init__()
-        self.is_built = False
-        self.metadata = {}
-        self.output_coding = coding
-        self.supported_codings = input_coding_types
-        self.name = name        
-        self.brick_tag = 'DensityRW'
-        self.net = None
-        self.graph = None
-        self.injection = None
-        self.transitions = transitions
-        self.init_walkers = init_walkers
-        self.timesteps = timesteps
-    
-    def build(self,
-              graph,
-              metadata,
-              control_nodes,
-              input_lists,
-              input_codings):
-        """
-        Build Density RW brick.
-
-        Arguments:
-            + graph - networkx graph to define connections of the computational graph
-            + metadata - dictionary to define the shapes and parameters of the brick
-            + control_nodes - dictionary of lists of auxillary networkx nodes.  Expected keys: 'complete' - A list of neurons that fire when the brick is done
-            + input_lists - list of nodes that will contain input
-            + input_coding - list of input coding formats.  All codings are allowed.
-
-        Returns:
-            + graph of a computational elements and connections
-            + dictionary of output parameters (shape, coding, layers, depth, etc)
-            + dictionary of control nodes ('complete')
-            + list of output (1 output)
-            + list of coding formats of output (Coding matches input coding)
-        """      
-        #Basic Checks
-        if len(input_lists) != 1:
-            raise ValueError('Incorrect Number of Inputs.')
-        for input_coding in input_codings:
-            if input_coding not in self.supported_codings:
-                raise ValueError("Unsupported Input Coding. Found: {}. Allowed: {}".format(input_coding,
-                                                                                           self.supported_codings))       
-        
-        #Complete Node
-        control_node_list = []
-        control_node_name = self.name + '_complete'
-        graph.add_node(control_node_name,
-                       index=(-1,),
-                       decay=0.0,
-                       potential=0.0,
-                       threshold = float(self.timesteps)
-                )
-        graph.add_edge(control_nodes[0]['complete'], control_node_name, weight=1.0, delay=1)
-        timestep_counter_name = self.name + '_timestep_counter'
-        graph.add_node(timestep_counter_name,
-                       index=(-2,),
-                       decay = 0.0,
-                       potential = 0.0,
-                       threshold = 0.5
-                       )
-        graph.add_edge(timestep_counter_name, timestep_counter_name, weight=1.0, delay=1)
-        graph.add_edge(timestep_counter_name, control_node_name, weight=1.0, delay=1)
-        
-        
-        #Build the walking graph
-        self.net = SpikingPDE.MarkovNetwork(initial_walkers=self.init_walkers,
-                            transitions=self.transitions,
-                            synchronized=True,
-                            log_potential=True, log_spikes=True)
-        self.graph = self.net.build()
-        self.graph = SpikingPDE.to_graph(self.graph, SpikingPDE.neuron_list)
-        
-        
-        #Prep initial position of walkers
-        counter_neurons = [node for node in self.graph.nodes if 'counter' in self.graph.nodes[node]['groups']]
-        for init_source in self.init_walkers:
-            for node in counter_neurons:
-                if str(init_source) in self.graph.nodes[node]['groups']:
-                    self.graph.nodes[node]['potential']= -self.init_walkers[init_source]
-        
-        for (neuron, current) in self.net.injection.get(0, []):
-            if neuron is self.net.walker_supervisor:
-                self.graph.nodes[neuron.name]['potential'] = current
-                
-        #Rename nodes
-        relabel_dictionary = {}
-        for node in self.graph.nodes:
-            relabel_dictionary[node] = self.name + '_' + str(node) + '_groups_' + str(self.graph.nodes[node]['groups'])
-        self.graph = nx.relabel_nodes(self.graph, relabel_dictionary, copy=False)
-        
-        #Grab references to output nodes
-        output_nodes = [node for node in self.graph.nodes if 'readout' in self.graph.nodes[node]['groups']]
-        
-        #Write indices for outputs (also bad code)
-        for node in output_nodes:
-            self.graph.nodes[node]['index'] = ast.literal_eval(self.graph.nodes[node]['groups'][1])
-
-        #Add RW nodes to Fugu graph (This is potentially memory intensive)
-        graph.update(self.graph)
-
-        self.is_built = True
-        return (graph, self.metadata, [{'complete':control_node_name}], [output_nodes], [self.output_coding])
-
-def load_transitions(mat_file,
-                remove_sink_connections=True,
-                verbose=1):
-    SpikingPDE.reset_neuron_list()
-    if '.mat' in mat_file:
-        prob_mtx = loadmat(mat_file)
-        prob_mtx = prob_mtx[mat_file[:-4]]
-    elif '.csv' in mat_file:
-        prob_mtx = genfromtxt(mat_file, delimiter=',')
-    else:
-        raise ValueError("Matrix file incompatible")
-    N = np.shape(prob_mtx)
-    transitions = {}
-    for i in range(N[0]):
-        neighbors = []
-        p = 0
-        for j in range(N[1]):
-            prob_i_j = prob_mtx[i,j]
-            p += prob_i_j
-            if prob_i_j > 0:
-                neighbors.append(((j,), prob_mtx[i,j]))
-        if verbose>0:
-            rw_logger.info("node " + str(i) + " has total probability of " + str(p) )
-        if remove_sink_connections and len(neighbors)==1 and neighbors[0]==((i,),1.0):
-            if verbose>0:
-                rw_logger.info("Removing connections from a sink at " + str((i,)))
-            neighbors=[]
-        transitions[(i,)] = SpikingPDE.Transition(location=(i,), neighbors=neighbors)
-    return transitions
+from fugu.bricks import Brick
+from fugu.scaffold.port import ChannelSpec, PortSpec, PortUtil
 
 
-def run_miniapp(transitions_file=None,
-                neural_timesteps = 100,
-                initial_walkers = [],
-                sink_connections=False,
-                backend='snn'):
-  transitions = load_transitions(transitions_file,
-                                 remove_sink_connections = not(sink_connections),
-                                 verbose=0)
-  scaffold = Scaffold()
-  if backend == 'snn':
-    from ..backends import snn_Backend
-    backend_object = snn_Backend()
-  elif backend == 'loihi':
-    from ..backends import loihi_Backend
-    backend_object = loihi_Backend()
-  else:
-    raise ValueError("Unsupported backend type.")
-  initial_walkers = { (t[0],):t[1] for t in initial_walkers}
-  scaffold.add_brick(Vector_Input(np.array([1]),coding='Raster',name='Input0'),'input')
-  scaffold.add_brick(DensityRW(neural_timesteps, 
-                               transitions,
-                               init_walkers = initial_walkers,
-                               name = 'DensityRW'),
-                    [0],
-                    output=True)
-  scaffold.lay_bricks()
-  
-  neuron_number_map = dict()
-  rw_neurons = [node for node in scaffold.graph.nodes if ('groups' in scaffold.graph.nodes[node])]
-  readout_neurons = [node for node in rw_neurons if ('readout' in scaffold.graph.nodes[node]['groups'])]
-  for neuron in readout_neurons:
-    neuron_number_map[scaffold.graph.nodes[neuron]['neuron_number']] = scaffold.graph.nodes[neuron]['groups'][1]
-  backend_object.compile(scaffold)
-  result = backend_object.run(neural_timesteps)
-  node_col = []
-  for neuron in result['neuron_number'].astype(int):
-    node_col.append(neuron_number_map[neuron])
-  result['node'] = node_col
-  return result
+class RandomWalker(Brick):
+    def __init__(
+        self,
+        transition_matrix,
+        init_walkers,
+        timesteps, 
+        name="RandomWalker",
+    ):
+        super().__init__(name=name)
+        self.P = np.asarray(transition_matrix, dtype=float)
+
+        if self.P.ndim != 2 or self.P.shape[0] != self.P.shape[1]:
+            raise ValueError("transition_matrix must be a square 2-D array.")
+        self.N = self.P.shape[0]        
+        if self.N == 0:
+            raise ValueError("transition_matrix must have at least one room.")
+        if np.any(self.P < 0):
+            raise ValueError("transition_matrix entries must be non-negative.")
+
+        self.timesteps = int(timesteps)
+        if self.timesteps < 1:
+            raise ValueError("timesteps must be >= 1.")
+
+        self.init_walkers = {int(k): int(v) for k, v in init_walkers.items()}
+        if not self.init_walkers:
+            raise ValueError("init_walkers must seed at least one walker.")
+        for room, count in self.init_walkers.items():
+            if not (0 <= room < self.N):
+                raise ValueError(f"init_walkers references room {room} outside 0..{self.N - 1}.")
+            if count <= 0:
+                raise ValueError(f"walker count for room {room} must be positive.")
+
+        self.supported_codings = ['Raster', 'Undefined']
+
+    @classmethod
+    def input_ports(cls):
+        """Source brick: takes no spike inputs (problem data comes via __init__)."""
+        return {}
+
+    @classmethod
+    def output_ports(cls):
+        port = PortSpec(name='output')
+        port.channels['data'] = ChannelSpec(name='data', coding=['Raster'])
+        port.channels['complete'] = ChannelSpec(name='complete')
+        return {port.name: port}
+
+    def build2(self, graph, inputs={}):
+        rooms = {}
+        for i in range(self.N):
+            neighbours = self._row_to_neighbours(i)
+            rooms[i] = self._add_room(graph, i, neighbours)
+
+        ctrl = self._add_controllers(graph)
+
+        self._wire_rooms(graph, rooms, ctrl)
+
+        self._connect_rooms(graph, rooms)
+
+        self._seed_walkers(graph, rooms, ctrl)
+
+        complete = self._add_completion(graph)
+
+        readout_names = [rooms[i]['readout'] for i in range(self.N)]
+        result = PortUtil.make_ports_from_specs(RandomWalker.output_ports())
+        out = result['output']
+        out.channels['data'].neurons = readout_names
+        out.channels['complete'].neurons = [complete]
+        return result
+
+    def _add_room(self, graph, i, neighbours):
+        idx = (i,)
+        probability_bits = max(len(neighbours) - 1, 1)
+
+        counter = self._room_neuron(i, 'counter')
+        graph.add_node(counter, index=idx, threshold=0.5, decay=0.0, p=1.0, potential=0.0)
+
+        generator = self._room_neuron(i, 'generator')
+        graph.add_node(generator, index=idx, threshold=0.5, decay=1.0, p=1.0, potential=0.0)
+
+        readout = self._room_neuron(i, 'readout')
+        graph.add_node(readout, index=idx, threshold=0.5, decay=0.0, p=1.0, potential=0.0)
+
+        buffer = self._room_neuron(i, 'buffer')
+        graph.add_node(buffer, index=idx, threshold=0.5, decay=0.0, p=1.0, potential=0.0)
+
+        buffer_control = self._room_neuron(i, 'buffer_control')
+        graph.add_node(buffer_control, index=idx, threshold=0.5, decay=1.0, p=1.0, potential=0.0)
+
+        probs = [p for (_, p) in neighbours] or [0.0]
+        random_gates = []
+        for k in range(probability_bits):
+            denom = 1.0 - sum(probs[:k])
+            p_k = (probs[k] / denom) if denom > 0 else 0.0
+            g = self._room_neuron(i, f'gate{k}')
+            graph.add_node(g, index=idx, threshold=0.5, decay=1.0, p=p_k, potential=0.0)
+            random_gates.append(g)
+
+        output_gates = []
+        for k in range(probability_bits + 1):
+            g = self._room_neuron(i, f'outgate{k}')
+            graph.add_node(g, index=idx, threshold=0.5, decay=1.0, p=1.0, potential=0.0)
+            output_gates.append(g)
+
+        return {
+            'neighbours': neighbours,
+            'counter': counter,
+            'generator': generator,
+            'readout': readout,
+            'buffer': buffer,
+            'buffer_control': buffer_control,
+            'random_gates': random_gates,
+            'output_gates': output_gates,
+        }
+
+    def _wire_rooms(self, graph, rooms, ctrl):
+        for room in rooms.values():
+            counter = room['counter']
+            generator = room['generator']
+            readout = room['readout']
+            buffer = room['buffer']
+            buffer_control = room['buffer_control']
+            gates = room['random_gates']
+            outs = room['output_gates']
+            output_length = len(outs)
+
+            graph.add_edge(buffer_control, counter,        weight=-1.0, delay=1)
+            graph.add_edge(buffer_control, buffer_control, weight=1.0,  delay=1)
+            graph.add_edge(buffer_control, buffer,         weight=1.0,  delay=1)
+            graph.add_edge(buffer,         counter,        weight=1.0,  delay=1)
+            graph.add_edge(buffer,         buffer_control, weight=-1.0, delay=1)
+            graph.add_edge(buffer,         buffer,         weight=-1.0, delay=1)
+            graph.add_edge(ctrl['buffer_supervisor'], buffer,         weight=1.0, delay=1)
+            graph.add_edge(ctrl['buffer_supervisor'], buffer_control, weight=1.0, delay=1)
+            graph.add_edge(buffer, ctrl['buffer_clear'], weight=1.0, delay=1)
+
+            graph.add_edge(generator, readout,   weight=1.0,  delay=1)
+            graph.add_edge(generator, generator, weight=1.0,  delay=1)
+            graph.add_edge(generator, counter,   weight=1.0,  delay=1)
+            graph.add_edge(counter,   readout,   weight=-1.0, delay=1)
+            graph.add_edge(counter,   counter,   weight=-1.0, delay=1)
+            graph.add_edge(counter,   generator, weight=-1.0, delay=1)
+
+            for k in range(output_length - 1):
+                graph.add_edge(generator, gates[k], weight=1.0,  delay=k + 1)
+                graph.add_edge(counter,   gates[k], weight=-1.0, delay=k + 1)
+                graph.add_edge(gates[k],  outs[k],  weight=1.0,  delay=1)
+                d = 1
+                for j in range(k + 1, output_length - 1):
+                    graph.add_edge(gates[k], gates[j], weight=-1.0, delay=d)
+                    d += 1
+                graph.add_edge(gates[k], outs[-1], weight=-1.0, delay=d)
+            graph.add_edge(generator, outs[-1], weight=1.0,  delay=output_length)
+            graph.add_edge(counter,   outs[-1], weight=-1.0, delay=output_length)
+
+            graph.add_edge(ctrl['walker_supervisor'], counter,   weight=1.0, delay=1)
+            graph.add_edge(ctrl['walker_supervisor'], generator, weight=1.0, delay=1)
+            graph.add_edge(counter, ctrl['walks_complete'], weight=1.0, delay=output_length)
+
+    def _connect_rooms(self, graph, rooms):
+        for room in rooms.values():
+            for k, (j, _p) in enumerate(room['neighbours']):
+                gate = room['output_gates'][k]
+                graph.add_edge(gate, rooms[j]['buffer'], weight=-1.0, delay=1)
+
+    def _add_controllers(self, graph):
+        g = self.generate_neuron_name
+        walker_supervisor = g('walker_supervisor')
+        walks_complete = g('walks_complete')
+        buffer_supervisor = g('buffer_supervisor')
+        buffer_clear = g('buffer_clear')
+
+        graph.add_node(walker_supervisor, threshold=1.0, decay=1.0, p=1.0, potential=0.0)
+        graph.add_node(buffer_supervisor, threshold=1.0, decay=1.0, p=1.0, potential=0.0)
+        graph.add_node(walks_complete, threshold=self.N - 0.5, decay=0.0, p=1.0, potential=0.0)
+        graph.add_node(buffer_clear,   threshold=self.N - 0.5, decay=0.0, p=1.0, potential=0.0)
+
+        graph.add_edge(walks_complete, buffer_supervisor, weight=2.0, delay=2)
+        graph.add_edge(buffer_clear,   walker_supervisor, weight=2.0, delay=2)
+
+        return {
+            'walker_supervisor': walker_supervisor,
+            'walks_complete': walks_complete,
+            'buffer_supervisor': buffer_supervisor,
+            'buffer_clear': buffer_clear,
+        }
+
+    def _seed_walkers(self, graph, rooms, ctrl):
+        graph.nodes[ctrl['walker_supervisor']]['potential'] = 10.0
+        for room, count in self.init_walkers.items():
+            graph.nodes[rooms[room]['counter']]['potential'] = -float(count)
+
+    def _add_completion(self, graph):
+        g = self.generate_neuron_name
+        complete = g('complete')
+        timestep_counter = g('timestep_counter')
+
+        graph.add_node(complete, index=(-1,), threshold=float(self.timesteps),
+                       decay=0.0, p=1.0, potential=0.0)
+        graph.add_node(timestep_counter, index=(-2,), threshold=0.5,
+                       decay=0.0, p=1.0, potential=1.0)
+        graph.add_edge(timestep_counter, timestep_counter, weight=1.0, delay=1)
+        graph.add_edge(timestep_counter, complete, weight=1.0, delay=1)
+        return complete
+
+    def _room_neuron(self, i, suffix):
+        return self.generate_neuron_name(f'room{i}_{suffix}')
+
+    def _row_to_neighbours(self, i):
+        return [(j, float(self.P[i, j])) for j in range(self.N) if self.P[i, j] > 0]
